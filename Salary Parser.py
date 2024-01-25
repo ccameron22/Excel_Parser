1902951dd01ca0f5f7cbb79c82ed7b3821d1cf4d
chasecameron <chasecameron@finfolio.com>
2024-01-25 17:10:57 -0600

    Added commments

--- Salary Parser.py
+++ Salary Parser.py
@@ -1,13 +1,15 @@
-# import pandas lib as pd
+# Import pandas lib as pd
 import pandas as pd
 
-# read by default 1st sheet of an excel file
+# Read excel holding base employee information
 dataframe1 = pd.read_excel('EmployeeInformation.xlsx')
 
 #print(dataframe1)
 #print(dataframe1.columns.ravel())
 
+# Create dictionary to hold count of employees in each state
 stateCount = {}
+# Create alphabetized list of each state
 states = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado",
   "Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois",
   "Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland",
@@ -17,6 +19,10 @@ states = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado",
   "Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah",
   "Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
 
+# Loop through each entry in the State column of the excel sheet
+# If the state has not been seen before, create an entry for it
+#   in the dictionary stateCount and set it's value to 1
+#   If it has been seen before increment it's value by 1
 for i in dataframe1['State']:
     if i in stateCount:
         x = stateCount[i]
