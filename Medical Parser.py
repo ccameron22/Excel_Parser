ed655028d5be1863e469e2fe8bb82b5d1cc14b03
chasecameron <chasecameron@finfolio.com>
2024-01-26 17:57:58 -0600

    Created dictionaries to assist with searching for matches when comparing Medical Records vs Insurance Records and comparing hits from Insurance Records against Voter Information.
    This prints the names of individuals that were identified

--- Medical Parser.py
+++ Medical Parser.py
@@ -12,55 +12,49 @@ voterBirthDateList = []
 dupVoterBirthDateList = []
 insuranceClaimDOBZIP = []
 dupInsuranceClaimDOBZIP = []
+insuranceDates = {}
+voterInfo = {}
 
-"""
+
 # In sheet of Voter information, combine first and last names into a new column
 voterHistoryNames['Full Name'] = voterHistoryNames["First Name"] + " " + voterHistoryNames["Last Name"]
-
+"""
 # Loop through newly created column in the Voter sheet and make a list of names
 for i in voterHistoryNames["Full Name"]:
     if i not in voterNames:
         voterNames.append(i)
 """
-
-# Loop through the voter information sheet and parse dates of birth, then convert to strings instead of time stamps
-for i in voterHistoryNames["Date of Birth"]:
-    x = i.strftime('%m/%d/%Y')
-    if x not in voterBirthDateList:
-        voterBirthDateList.append(x)
-    else:
-        dupVoterBirthDateList.append(x)
+# Create new column in the voter history sheet to combine DOB and ZIP of voter
+voterHistoryNames["Date of Birth"] = voterHistoryNames["Date of Birth"].dt.strftime('%m/%d/%Y')
+voterHistoryNames["Zip"] = voterHistoryNames["Zip"].map(str)
+voterHistoryNames['DOB ZIP'] = voterHistoryNames["Date of Birth"] + " " + voterHistoryNames["Zip"]
 
 # Create new column in the insurance sheet to combine DOB and ZIP of patient
 insuranceClaims["Date of Birth"] = insuranceClaims["Date of Birth"].dt.strftime('%m/%d/%Y')
 insuranceClaims["Zip"] = insuranceClaims["Zip"].map(str)
 insuranceClaims['DOB ZIP'] = insuranceClaims["Date of Birth"] + " " + insuranceClaims["Zip"]
 
-# Loop through the insurance claim information sheet and parse dates of birth, then convert to strings instead of
-# time stamps. Combine this with the zipcode to create a potentially identifiable string
-for i in insuranceClaims["DOB ZIP"]:
-    if i not in insuranceClaimDOBZIP:
-        insuranceClaimDOBZIP.append(i)
-    else:
-        dupInsuranceClaimDOBZIP.append(i)
+# Format date of service in Medical record and Insurance record
+medicalTreatment["Date of Service"] = medicalTreatment["Date of Service"].dt.strftime('%m/%d/%Y')
+insuranceClaims["Date of Service"] = insuranceClaims["Date of Service"].dt.strftime('%m/%d/%Y')
 
-print("Voter DOBs")
-print(voterBirthDateList)
-print(dupVoterBirthDateList)
-print()
-print("Insurance DOBs")
-print(insuranceClaimDOBZIP)
-print(dupInsuranceClaimDOBZIP)
+# Create dictionary with Date of Service as key and DOB-ZIP as value
+index = 0
+for i in insuranceClaims["Date of Service"]:
+    insuranceDates[i] = insuranceClaims.loc[index, "DOB ZIP"]
+    index += 1
 
-"""
-# Loop through each entry in the State column of the excel sheet
-# If the state has not been seen before, create an entry for it
-#   in the dictionary stateCount and set it's value to 1
-#   If it has been seen before increment it's value by 1
-for i in dataframe1['State']:
-    if i in stateCount:
-        x = stateCount[i]
-        stateCount[i] = x+1
-    else:
-        stateCount[i] = 1
-"""
+# Create dictionary with DOB-Zip as key and Full Name as value
+index = 0
+for i in voterHistoryNames["DOB ZIP"]:
+    voterInfo[i] = voterHistoryNames.loc[index, "Full Name"]
+    index += 1
+
+# Loop through each entry in the Date of Service column
+# of the Medical Treatment record and check if a matching date of treatment
+for i in medicalTreatment["Date of Service"]:
+    if i in insuranceDates:
+        x = insuranceDates[i]
+        if x in voterInfo:
+            y = voterInfo[x]
+            print(y)
