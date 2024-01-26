ec40d5493a7fdacae62ad4d6a9c12b8351863167
chasecameron <chasecameron@finfolio.com>
2024-01-26 16:28:51 -0600

    Added lines to filter Insurance claim data and combine DOB with Zip to assist in filtering for identifying procedures in medical sheet

--- Medical Parser.py
+++ Medical Parser.py
@@ -2,19 +2,18 @@
 import pandas as pd
 
 # Read excel with medical and voter information
-medicalTreatmentNames = pd.read_excel('MedicalHistory.xlsx', sheet_name="Medical History")
+medicalTreatment = pd.read_excel('MedicalHistory.xlsx', sheet_name="Medical History")
 voterHistoryNames = pd.read_excel('MedicalHistory.xlsx', sheet_name="Voter Registration")
+insuranceClaims = pd.read_excel('MedicalHistory.xlsx', sheet_name="Insurance Claims")
 
 # Create lists to hold parsed columns
-medNames = []
 voterNames = []
-birthDateList = []
+voterBirthDateList = []
+dupVoterBirthDateList = []
+insuranceClaimDOBZIP = []
+dupInsuranceClaimDOBZIP = []
 
-# Loop through sheet of medical providers and strip names
-for i in medicalTreatmentNames['Provider']:
-    if i not in medNames:
-        medNames.append(i)
-
+"""
 # In sheet of Voter information, combine first and last names into a new column
 voterHistoryNames['Full Name'] = voterHistoryNames["First Name"] + " " + voterHistoryNames["Last Name"]
 
@@ -22,14 +21,37 @@ voterHistoryNames['Full Name'] = voterHistoryNames["First Name"] + " " + voterHi
 for i in voterHistoryNames["Full Name"]:
     if i not in voterNames:
         voterNames.append(i)
+"""
 
 # Loop through the voter information sheet and parse dates of birth, then convert to strings instead of time stamps
 for i in voterHistoryNames["Date of Birth"]:
     x = i.strftime('%m/%d/%Y')
-    if x not in birthDateList:
-        birthDateList.append(x)
+    if x not in voterBirthDateList:
+        voterBirthDateList.append(x)
+    else:
+        dupVoterBirthDateList.append(x)
 
-print(birthDateList)
+# Create new column in the insurance sheet to combine DOB and ZIP of patient
+insuranceClaims["Date of Birth"] = insuranceClaims["Date of Birth"].dt.strftime('%m/%d/%Y')
+insuranceClaims["Zip"] = insuranceClaims["Zip"].map(str)
+insuranceClaims['DOB ZIP'] = insuranceClaims["Date of Birth"] + " " + insuranceClaims["Zip"]
+
+# Loop through the insurance claim information sheet and parse dates of birth, then convert to strings instead of
+# time stamps. Combine this with the zipcode to create a potentially identifiable string
+for i in insuranceClaims["DOB ZIP"]:
+    if i not in insuranceClaimDOBZIP:
+        insuranceClaimDOBZIP.append(i)
+    else:
+        dupInsuranceClaimDOBZIP.append(i)
+
+print("Voter DOBs")
+print(voterBirthDateList)
+print(dupVoterBirthDateList)
+print()
+print("Insurance DOBs")
+print(insuranceClaimDOBZIP)
+print(dupInsuranceClaimDOBZIP)
+
 """
 # Loop through each entry in the State column of the excel sheet
 # If the state has not been seen before, create an entry for it
